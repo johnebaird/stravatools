23b67436eb43699cec18af6f908e2ab45d5230b2
John Baird <96591514+johnebaird@users.noreply.github.com>
2023-01-05 16:17:06 -0400

    changes to support maintenance texts

--- src/main/java/com/stravatools/main/MaintenanceRepository.java
+++ src/main/java/com/stravatools/main/MaintenanceRepository.java
@@ -1,9 +1,13 @@
 package com.stravatools.main;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MaintenanceRepository extends CassandraRepository<Maintenance, UUID> {}
+public interface MaintenanceRepository extends CassandraRepository<Maintenance, UUID> {
+
+    List<Maintenance> findByUsername(String username);
+}
