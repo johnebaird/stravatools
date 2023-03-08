92941cbf6995f094803419c4479404460b412044
John Baird <96591514+johnebaird@users.noreply.github.com>
2023-03-08 13:51:12 -0400

    bootstrap / crispy integration

--- django/stravatools/settings.py
+++ django/stravatools/settings.py
@@ -41,7 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_bootstrap5',
+    'crispy_forms',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
